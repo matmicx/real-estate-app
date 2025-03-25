c0281de022a7ef56ee1e5ae9a1de75256aab9049
matmicx <mat@michbniewski.com>
2025-03-25 16:08:59 +0000

    add google authentication

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-3xl my-10 font-rubik ">
+        Welcome to the Real Estate
+      </Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+const SignIn = () => {
+  const handleLogin = () => {
+    console.log("Login");
+  };
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to ReState
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-100 rounded-full w-full"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
